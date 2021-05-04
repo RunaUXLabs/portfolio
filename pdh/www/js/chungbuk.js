a2f45199a6852a3c0ebc6134d802ce5b3e40b6f3
RunaUXLabs <73094198+RunaUXLabs@users.noreply.github.com>
2021-05-04 17:27:08 +0900

    김윤정박다희 포트폴리오 업데이트

--- pdh/www/js/chungbuk.js
+++ pdh/www/js/chungbuk.js
@@ -77,3 +77,53 @@ var swiper3 = new Swiper('.swiper-container.bottom', {
         prevEl: '.bottom .swiper-button-prev',
     },
 });
+
+
+
+var swiper4 = new Swiper('.swiper-container.doc', {
+  spaceBetween: 30,
+    slidesPerView: 1,
+    // autoplay: {
+    //     delay: 3500,
+    //     disableOnInteraction: false,
+    // },
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: '.doc .swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.doc .swiper-button-next',
+        prevEl: '.doc .swiper-button-prev',
+    },
+});
+
+$(function() {
+  $('.oneday').click(function() {
+
+      var chkd = $("#pop_day").is(":checked");
+      if(chkd){
+          $.cookie('popup', 'hidden', {expires : 1});
+          // jquery.cookie에서 $기호를 변수이름처럼 사용하였다. expires는 jquery.cookie에서 선언되었다.
+          // 스크립트의 이름이 jquery. (혹은 $.)으로 시작하는 것은 이 스크립트가 jQuery에 의존적이하는 점을 명시하는것이다.
+      }
+      $('.news').css('display', 'none');
+      // 1일동안 닫기라는 버튼을 눌렀을때 (체크박스)쿠키 적용 여부 판단하여 출력여부 판단
+  });
+
+  if($.cookie('popup') == 'hidden'){
+      $('.news').css('display', 'none');
+  }else{
+      $('.news').css('display', 'block');
+  }
+  // 팝업의 출력형태를 분석해서, 이게 쿠키때문인지, 그냥 x눌러서 안보이게 한건지 구분해서 재처리
+
+
+  //닫기버튼  
+  $('.ic_close').click(function() {
+      // $('.popup').hide();
+      $('.news').css('display', 'none');
+  });
+  
+});
